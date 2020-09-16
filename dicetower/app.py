35e5649227ec5777684f61adbc5916d1db3de3f7
Daniel Wallace <danielwallace@gtmanfred.com>
2020-09-16 10:38:25 -0500

    add static files to dicetower

--- dicetower/app.py
+++ dicetower/app.py
@@ -1,4 +1,4 @@
-import fastapi
+import fastapi.staticfiles
 import socketio
 
 from . import __version__
@@ -22,6 +22,21 @@ def create_app(debug=False):
 
     app.add_route("/socket.io/", route=sio_asgi_app, methods=['GET', 'POST'])
     app.add_websocket_route("/socket.io/", sio_asgi_app)
+    app.mount(
+        '/html',
+        fastapi.staticfiles.StaticFiles(directory='dicetower/static/html', html=True),
+        name='html',
+    )
+    app.mount(
+        '/js',
+        fastapi.staticfiles.StaticFiles(directory='dicetower/static/js', html=True),
+        name='javascript',
+    )
+    app.mount(
+        '/css',
+        fastapi.staticfiles.StaticFiles(directory='dicetower/static/css', html=True),
+        name='styles',
+    )
 
     load_handlers(app)
 
