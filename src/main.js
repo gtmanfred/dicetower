b209ba678ef680e31edbec4a9397ded73ae6c1de
Daniel Wallace <danielwallace@gtmanfred.com>
2020-09-24 08:30:38 -0500

    add BootstrapVue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import {
   BootstrapVue,
   ButtonPlugin
 } from 'bootstrap-vue'
+//import './main.css'
 
 Vue.config.productionTip = process.env.NODE_ENV === 'production'
 Vue.use(BootstrapVue)
